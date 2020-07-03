ff2c8235417304e5b8b6d657c47dae3065c56e03
Archi711 <mlody711@o2.pl>
2020-07-03 15:10:14 +0200

    task 1 - done

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 /* Here goes your JS code */
+
+const handleInitialLoad = event => {
+  const showPopupBtn = document.querySelector("button#show-popup-form")
+  showPopupBtn.addEventListener("click", togglePopupForm, false)
+  const hidePopupBtn = document.querySelector("button#hide-popup-form")
+  hidePopupBtn.addEventListener("click", togglePopupForm, false)
+}
+
+const togglePopupForm  = event => {
+  const popup = document.querySelector("div.popup")
+  popup.classList.toggle("popup-hidden")
+}
+
+window.addEventListener("DOMContentLoaded", handleInitialLoad)
